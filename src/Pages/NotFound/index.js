86d159ae5981548ce34bf02fdcb51f007d38a0a3
E Prasetyo <67994910+E-Prasetyo@users.noreply.github.com>
2022-09-21 12:36:57 +0700

    fix pages not found

--- src/Pages/NotFound/index.js
+++ src/Pages/NotFound/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='flex flex-col justify-center items-center h-screen text-2xl font-bold'>
+      NotFound
+    </div>
   )
 }
 
